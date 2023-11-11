1b34bdd3be0ae2241591bf13416d4e51820f6112
adibdehghan <developer@digitalfirst.media>
2023-11-11 12:01:36 +0200

    Initial Commit

---
+++ HorizonPower/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HorizonPower
+//
+//  Created by Adib Dehghan on 11/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
